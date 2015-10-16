50720d1f5982bf48860ab7fb80c24e541e3de061
Wade Novak <wade.novak@gmail.com>
2015-10-16 16:25:42 -0500

    Styles textarea fields for link previews

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,24 +2,22 @@ var selectAll  = document.getElementById('select-all-links') || [];
 var checkboxes = document.getElementsByClassName('link-checkbox');
 var linkPreviews = document.getElementsByClassName('link-preview');
 
-if (selectAll.length > 0) {
-  selectAll.addEventListener("click", function(event) {
+selectAll.addEventListener("click", function(event) {
 
-    if (this.innerText === "click here to select all of the links.") {
-      this.innerText = "click here to deselect all of the links.";
+  if (this.innerText === "click here to select all of the links.") {
+    this.innerText = "click here to deselect all of the links.";
 
-      for(var i=0; i<checkboxes.length; i++) {
-        checkboxes[i].checked = true;
-      }
-    } else {
-      this.innerText = "click here to select all of the links.";
-
-      for(var i=0; i<checkboxes.length; i++) {
-        checkboxes[i].checked = false;
-      }
+    for(var i=0; i<checkboxes.length; i++) {
+      checkboxes[i].checked = true;
     }
-  });
-}
+  } else {
+    this.innerText = "click here to select all of the links.";
+
+    for(var i=0; i<checkboxes.length; i++) {
+      checkboxes[i].checked = false;
+    }
+  }
+});
 
 function updateLinkPreviews() {
   for (var i=0; i<checkboxes.length; i++) {
